1a5fe0b9903056deaa2bbe1a7cb071dfc24b6bfa
guzhan11 <1003865572@qq.com>
2017-09-29 21:19:54 +0800

    9-29

--- src/store/index.js
+++ src/store/index.js
@@ -6,3 +6,28 @@ export default function configStore () {
   const store = createStore(rootReducer, applyMiddleware(promiseMiddleware))
   return store
 }
+
+// import { createStore, applyMiddleware, compose } from 'redux'
+// import promiseMiddleware from 'redux-promise'
+// import rootReducer from './reducers'
+// import devTools from '../util/remote-redux-devtools'
+//
+// // export default function configStore () {
+// //   const store = createStore(rootReducer, applyMiddleware(promiseMiddleware))
+// //   return store
+// // }
+// export default function configStore () {
+//   const store = createStore(
+//     rootReducer,
+//     compose(
+//       devTools({
+//         hostname: 'localhost',
+//         port: 5678,
+//         secure: false
+//       }),
+//       applyMiddleware(promiseMiddleware)
+//     )
+//   )
+//   console.info(store.getState())
+//   return store
+// }
